dc38be1e3781ab9b8a2371cabcf1d1d9e0d6fa1b
Leonardo Grasso <me@leonardograsso.com>
2016-04-28 11:54:06 +0200

    Copyright (c) 2015-2016 [ci skip]

--- src/Model/MatryoshkaConnectedResourceInterface.php
+++ src/Model/MatryoshkaConnectedResourceInterface.php
@@ -3,7 +3,7 @@
  * Matryoshka Connected Resource for Apigility
  *
  * @link        https://github.com/matryoshka-model/zf-apigility-matryoshka
- * @copyright   Copyright (c) 2015, Ripa Club
+ * @copyright   Copyright (c) 2015-2016, Ripa Club
  * @license     http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
  */
 namespace Matryoshka\Apigility\Model;
